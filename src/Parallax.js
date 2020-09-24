950a2a04e85203bee781007817a05f60e69a1d2f
Tim Fischer <emailtimfischer@gmail.com>
2020-09-23 22:33:08 -0500

    line break

--- src/Parallax.js
+++ src/Parallax.js
@@ -3,6 +3,7 @@ import { Parallax } from 'react-spring/addons';
 import BatsLayer from './sections/BatsSection';
 import CitySection from './sections/CitySection';
 import CloudsLayer from './sections/CloudsSection';
+
 const topFade = `linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,25,121,1) 100%)`;
 
 class ParallaxDemo extends Component {
